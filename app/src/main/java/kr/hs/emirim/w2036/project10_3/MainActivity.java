440c2c2ab34d8e896e97bb1f8cb8d1e84806fe4f
유하람 <w2036@e-mirim.hs.kr>
2021-07-21 10:03:42 +0900

    Completion Project10_3 Commit

--- app/src/main/java/kr/hs/emirim/w2036/project10_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2036/project10_3/MainActivity.java
@@ -2,7 +2,10 @@ package kr.hs.emirim.w2036.project10_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("Activity Life Cycle: ", "Call onCreate()");
 
         Button btnCall = findViewById(R.id.btn_call);
         Button btnFinish = findViewById(R.id.btn_finish);
@@ -25,10 +30,38 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             switch(v.getId()) {
                 case R.id.btn_call:
+                    Uri uri = Uri.parse("tel:01011112222");
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    startActivity(intent);
                     break;
                 case R.id.btn_finish:
+                    finish();
                     break;
             } //end of switch
         }
     };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Activity Life Cycle: ", "Call onDestroy()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Activity Life Cycle: ", "Call onRestart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Activity Life Cycle: ", "Call onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Activity Life Cycle: ", "Call onPause()");
+    }
 }
