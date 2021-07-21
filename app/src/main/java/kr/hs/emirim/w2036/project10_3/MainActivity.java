c12f4d2d6626276c710a327bde001779d30eaaf1
유하람 <w2036@e-mirim.hs.kr>
2021-07-21 09:27:24 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/w2036/project10_3/MainActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.w2036.project10_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
